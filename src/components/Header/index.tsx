512031713b2d4b6d568ff61dc5fdf88fb834bf96
Rafael Freire <rafaeldacostafreire@gmail.com>
2023-05-01 19:58:51 -0300

    style: :lipstick: Styled Components added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Label>Header</S.Label>
+    </S.Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100px;
+
+  background-color: #2d2d2d;
+`;
+
+export const Label = styled.div`
+  color: #ffffff;
+`;
