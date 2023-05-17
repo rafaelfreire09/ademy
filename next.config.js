26b16401eb803fa17814a544135fed2e982db1f9
Rafael Freire <rafaeldacostafreire@gmail.com>
2023-05-17 14:06:13 -0300

    chore: :wrench: added env variables with next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  env: {
+    API_BASE_URL: 'http://localhost:5000',
+  },
 }
 
 module.exports = nextConfig
