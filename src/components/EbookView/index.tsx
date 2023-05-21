eac28120e3b3f6b2f60aac55f914fde7b34c5a08
Rafael Freire <rafaeldacostafreire@gmail.com>
2023-05-21 18:02:14 -0300

    feat: :sparkles: add an ebook to cart

--- src/components/EbookView/index.tsx
+++ src/components/EbookView/index.tsx
@@ -1,14 +1,39 @@
 import * as S from './styles';
-
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+
+import { useAppDispatch } from 'hooks/redux';
+
 import Button from 'components/Button';
+
 import { Ebook } from 'types/types';
+import { addToCart } from 'redux/cartSlice';
+import { formatPrice } from 'utils/general';
 
 export type EbookViewProps = {
   ebook: Ebook;
 };
 
 export default function EbookView({ ebook }: EbookViewProps) {
+  const router = useRouter();
+
+  const dispatch = useAppDispatch();
+
+  const handleClickOnBuy = () => {
+    dispatch(
+      addToCart({
+        id: ebook.id,
+        title: ebook.title,
+        slug: ebook.slug,
+        image: ebook.image,
+        price: ebook.price,
+        quantity: 1,
+      })
+    );
+
+    router.push('/cart');
+  };
+
   return (
     <S.Wrapper>
       <S.ImageWrapper>
@@ -22,8 +47,14 @@ export default function EbookView({ ebook }: EbookViewProps) {
         <S.Description>{ebook.description}</S.Description>
       </S.Content>
       <S.CallToAction>
-        <S.Price>R${ebook.price}</S.Price>
-        <Button width="85" height="50" label="Comprar" colorType="green" />
+        <S.Price>{formatPrice(ebook.price)}</S.Price>
+        <Button
+          width="85"
+          height="50"
+          label="Comprar"
+          colorType="green"
+          onClick={handleClickOnBuy}
+        />
       </S.CallToAction>
     </S.Wrapper>
   );
