f026b55d752499aa31eda4165aa9d7ed5a06cba7
Rafael Freire <rafaeldacostafreire@gmail.com>
2023-06-14 14:26:27 -0300

    feat: :sparkles: new button to access purchased ebook link

--- src/components/EbookDashboardView/index.tsx
+++ src/components/EbookDashboardView/index.tsx
@@ -1,14 +1,26 @@
 import * as S from './styles';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import Button from 'components/Button';
 import { Ebook } from 'types/types';
+import { DownloadEbookBySrc } from 'services/ebook';
 
 export type EbookDashboardViewProps = {
   ebook: Ebook;
 };
 
 export default function EbookDashboardView({ ebook }: EbookDashboardViewProps) {
+  const router = useRouter();
+
+  const handleClickOnDownload = async () => {
+    const response = await DownloadEbookBySrc(ebook.slug)
+
+    if (response?.Src) {
+      router.push(response?.Src)
+    }
+  };
+
   return (
     <S.Wrapper>
       <S.ImageWrapper>
@@ -22,7 +34,13 @@ export default function EbookDashboardView({ ebook }: EbookDashboardViewProps) {
         <S.Description>{ebook.description}</S.Description>
       </S.Content>
       <S.CallToAction>
-        <Button width="85" height="50" label="Baixar" colorType="green" />
+        <Button
+          width="85"
+          height="50"
+          label="Baixar"
+          colorType="green"
+          onClick={handleClickOnDownload}
+        />
       </S.CallToAction>
     </S.Wrapper>
   );
